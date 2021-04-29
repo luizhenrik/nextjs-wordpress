ff8de141fdf544ea127d5f8df38d0b17495d04b5
luizhenrik <luhemeso@gmail.com>
2021-04-28 23:24:50 -0300

    arrumei espaçamentos e comecei servicos home

--- components/home/services.js
+++ components/home/services.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
+import style from '@/components/home/services.module.scss'
+
 export default function HomeServices() {
   return (
-        <div>{'sou serviço'}</div>
+        <div className={`${style['home-services']}`}>
+            <div className={`${style['home-services__content']}`}>
+                <h2 className={`${style['home-services__title']}`}>Fisioterapia e suas tecnicas</h2>
+                <h3 className={`${style['home-services__subtitle']}`}>Incididunt quis nostrud minim velit est dolore deserunt id deserunt nulla sit commodo aute.</h3>
+
+                <div className={`${style['home-services__container']}`}>
+                    <div className={`${style['home-services__item']}`}>
+                        <h4 className={`${style['home-services__item-title']}`}>Tecnica #1</h4>
+                        <p className={`${style['home-services__item-text']}`}>Aliqua anim ut do enim consequat velit laboris nostrud Lorem deserunt do laboris. Voluptate sit anim Lorem mollit esse eiusmod dolore dolor cupidatat adipisicing.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
   )
 }
