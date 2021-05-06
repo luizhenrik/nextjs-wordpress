0ba9517b9dac602c7fba1164a26d352be39ea2cc
luizhenrik <luhemeso@gmail.com>
2021-05-06 07:58:36 -0300

    fix styles

--- components/home/services.js
+++ components/home/services.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import style from '@/components/home/services.module.scss'
-import Image from 'next/image'
 
 export default function HomeServices() {
   return (
@@ -12,32 +11,14 @@ export default function HomeServices() {
 
                 <div className={`${style['home-services__container']}`}>
                     <div className={`${style['home-services__item']}`}>
-                        <Image
-                        src={'/images/home-bio.png'}
-                        width={480}
-                        height={480}
-                        className={`${style['home-services__item-image']}`}
-                        />
                         <h4 className={`${style['home-services__item-title']}`}>Tecnica #1</h4>
                         <p className={`${style['home-services__item-text']}`}>Ad elit exercitation ex adipisicing aute eiusmod eiusmod reprehenderit. Aute aliquip aliquip eiusmod quis deserunt ipsum adipisicing aliqua ipsum amet do Lorem sunt ex. Quis consequat et qui sunt pariatur nostrud. Consequat mollit aliqua voluptate commodo minim. Do magna culpa nisi labore in magna cupidatat consectetur eiusmod.</p>
                     </div>
                     <div className={`${style['home-services__item']}`}>
-                        <Image
-                        src={'/images/home-bio.png'}
-                        width={480}
-                        height={480}
-                        className={`${style['home-services__item-image']}`}
-                        />
                         <h4 className={`${style['home-services__item-title']}`}>Tecnica #2</h4>
                         <p className={`${style['home-services__item-text']}`}>Cupidatat est ea laboris consequat nulla tempor mollit quis qui culpa labore voluptate ex nisi. Deserunt eiusmod incididunt commodo minim in dolor. Nulla id ea ex officia proident do do Lorem irure aute occaecat velit ad sint. Dolor consequat in qui excepteur et ex incididunt reprehenderit nisi in.</p>
                     </div>
                     <div className={`${style['home-services__item']}`}>
-                        <Image
-                        src={'/images/home-bio.png'}
-                        width={480}
-                        height={480}
-                        className={`${style['home-services__item-image']}`}
-                        />
                         <h4 className={`${style['home-services__item-title']}`}>Tecnica #3</h4>
                         <p className={`${style['home-services__item-text']}`}>Ipsum in dolore eu cillum commodo ex aliquip. Occaecat excepteur nisi dolor culpa excepteur do aliqua ea ex deserunt magna ad. Veniam ullamco laborum irure non excepteur occaecat. Duis est veniam in deserunt Lorem pariatur magna cupidatat dolore exercitation reprehenderit proident sint dolor. Incididunt cupidatat magna anim anim dolor nisi pariatur nulla cupidatat est.</p>
                     </div>
